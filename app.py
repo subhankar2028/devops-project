71cdad2eaccc15b1c03d8c530fc08bc1fed99fa9
Subhankar Majumder <subhankar.m6195@gmail.com>
2024-04-08 00:37:48 +0530

    index route added

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ def hello_world():
     time.sleep(2)
     return "Machine ID : " + str(machine_id)
 
+@app.route("/index")
+def hello_world():
+    machine_id = uuid.getnode()
+    time.sleep(2)
+    return "This is index page"
+
 if __name__ == "__main__":
     app.run(threaded=True)
